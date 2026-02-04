594062ce5f1e5010691b585926e0ad81cb45f30a
sciFilm <thitipong.tam@ku.th>
2026-02-04 18:11:59 +0700

    Fixed the app crashing in IOS when trying to sign in with Google (separated the IDs for both platforms)

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io' show Platform;
+
 import 'package:firebase_auth/firebase_auth.dart' show AuthCredential;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
@@ -6,12 +8,17 @@ import 'package:flutter/material.dart';
 class AuthenticationPage extends StatelessWidget {
   AuthenticationPage({super.key});
 
-  final List<AuthProvider<AuthListener, AuthCredential>> providers = [
-    GoogleProvider(
-      clientId:
-          "100421617321-b4bdb6opjkipqlk254baausjpdj4gcs9.apps.googleusercontent.com",
-    ),
-    EmailAuthProvider()
+  static const String _iosClientId =
+      "100421617321-3giqlnh4linad8utja6lcme2qkqe7imr.apps.googleusercontent.com";
+  static const String _androidClientId =
+      "100421617321-b4bdb6opjkipqlk254baausjpdj4gcs9.apps.googleusercontent.com";
+
+  static String get _clientId =>
+      Platform.isIOS ? _iosClientId : _androidClientId;
+
+  List<AuthProvider<AuthListener, AuthCredential>> get providers => [
+    GoogleProvider(clientId: _clientId),
+    EmailAuthProvider(),
   ];
 
   @override
